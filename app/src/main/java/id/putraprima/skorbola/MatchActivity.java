a292dbc716b96961e8d9c847c0c3f711ba9b1778
addinnt <addin309@gmail.com>
2020-02-24 15:43:07 +0700

    validasi + intent data (exc gambar)

--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -2,17 +2,62 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import id.putraprima.skorbola.Model.Model;
 
 public class MatchActivity extends AppCompatActivity {
+    private TextView homeName;
+    private TextView awayName;
+    private TextView homeScore;
+    private TextView awayScore;
+    private ImageView homeLogo;
+    private  ImageView awayLogo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+
+        //bind view
+        homeName = findViewById(R.id.txt_home);
+        awayName = findViewById(R.id.txt_away);
+        homeScore = findViewById(R.id.score_home);
+        awayScore = findViewById(R.id.score_away);
+        awayLogo = findViewById(R.id.away_logo);
+        homeLogo = findViewById(R.id.home_logo);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            Model model = extras.getParcelable(MainActivity.USER_KEY);
+            homeName.setText(model.getHomeName());
+            awayName.setText(model.getAwayName());
+//            homeScore.setText(model.getHomeScore());
+//            awayScore.setText(model.getAwayScore());
+            Bitmap bmp = BitmapFactory.decodeByteArray(getIntent().getByteArrayExtra("homeLogo"), 0, getIntent().getByteArrayExtra("homeLogo").length);
+            homeLogo.setImageBitmap(bmp);
+            Bitmap bmpAway = BitmapFactory.decodeByteArray(getIntent().getByteArrayExtra("awayLogo"), 0, getIntent().getByteArrayExtra("awayLogo").length);
+            awayLogo.setImageBitmap(bmpAway);
+        }
         //TODO
         //1.Menampilkan detail match sesuai data dari main activity
         //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
         //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
+    }
+
+    public void handleAddHomeScore(View view) {
+
+    }
+
+    public void handleAddAwayScore(View view) {
+    }
+
+    public void handleCekHasil(View view) {
     }
 }
---
+++ app/src/main/java/id/putraprima/skorbola/Model/Model.java
@@ -0,0 +1,88 @@
+package id.putraprima.skorbola.Model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Model implements Parcelable {
+
+    private String homeName;
+    private String awayName;
+    private int homeScore;
+    private int awayScore;
+
+    public Model(String homeName, String awayName) {
+        this.homeName = homeName;
+        this.awayName = awayName;
+    }
+
+    public Model(String homeName, String awayName, int homeScore, int awayScore) {
+        this.homeName = homeName;
+        this.awayName = awayName;
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+    }
+
+    protected Model(Parcel in) {
+        homeName = in.readString();
+        awayName = in.readString();
+        homeScore = in.readInt();
+        awayScore = in.readInt();
+    }
+
+    public static final Creator<Model> CREATOR = new Creator<Model>() {
+        @Override
+        public Model createFromParcel(Parcel in) {
+            return new Model(in);
+        }
+
+        @Override
+        public Model[] newArray(int size) {
+            return new Model[size];
+        }
+    };
+
+    public String getHomeName() {
+        return homeName;
+    }
+
+    public void setHomeName(String homeName) {
+        this.homeName = homeName;
+    }
+
+    public String getAwayName() {
+        return awayName;
+    }
+
+    public void setAwayName(String awayName) {
+        this.awayName = awayName;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public void setHomeScore(int homeScore) {
+        this.homeScore = homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    public void setAwayScore(int awayScore) {
+        this.awayScore = awayScore;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(homeName);
+        parcel.writeString(awayName);
+        parcel.writeInt(homeScore);
+        parcel.writeInt(awayScore);
+    }
+}
